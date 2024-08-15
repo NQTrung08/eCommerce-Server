58df0a4a43c9edb7173dcd31e9fee2beb39efc38
NQTrung08 <nqtrung0810@gmail.com>
2024-08-15 23:06:45 +0700

    create model otp and template send email

---
+++ src/models/otp.model.js
@@ -0,0 +1,19 @@
+// models/otp.model.js
+const mongoose = require('mongoose');
+
+const DOCUMENT_NAME = 'OTP'
+const COLLECTION_NAME = 'OTPs'
+
+const otpSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  otp_token: { type: String, required: true },
+  otp_status: { type: String, default: 'pending', enum: ['pending', 'active', 'block']},
+  createdAt: { type: Date, default: Date.now, expires: 300 }, // OTP sẽ hết hạn sau 5 phút
+}, {
+  collection: COLLECTION_NAME,
+  timestamps: true,
+});
+
+const OtpModel = mongoose.model(DOCUMENT_NAME, otpSchema);
+
+module.exports = OtpModel;
