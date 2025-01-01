ee92d73e41e5ff757fb5d84f7be3b1b8747910bc
NQTrung08 <nqtrung0810@gmail.com>
2025-01-01 11:08:42 +0700

    feat: deploy aws

--- src/configs/config.app.js
+++ src/configs/config.app.js
@@ -20,7 +20,7 @@ const pro = {
     port: process.env.DEV_APP_PORT || 3052,
     redirectUrl: process.env.PRO_REDIRECT_URL,
     callbackUrl: process.env.PRO_CALLBACK_URL,
-    url: 'https://ecommerce-server-0mcc.onrender.com',
+    url: process.env.PRO_URL,
     resetPasswordUrl: process.env.PRO_RESET_PASSWORD_LINK
   },
   vnpay: {
